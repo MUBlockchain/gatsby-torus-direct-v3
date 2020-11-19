67a21724d39eff04cdb9ae1f335f7cef1c251a33
Jack Gilcrest <gilcest.jack@gmail.com>
2020-11-19 15:22:22 -0500

    first push

---
+++ src/components/login.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import DirectWebSdk from '@toruslabs/torus-direct-web-sdk'
+
+const torus = new DirectWebSdk({
+  baseUrl: 'http://localhost:8000/serviceworker/',
+  // proxyContractAddress: '0x4023d2a0D330bF11426B12C6144Cfb96B7fa6183', // details for test net
+  // network: 'ropsten', // details for test net,
+  enableLogging: true
+})
+
+async function initTorus () {
+  await torus.init()
+}
+
+async function login () {
+  console.log('in login')
+
+  await initTorus()
+
+  const userInfo = await torus.triggerLogin({
+    verifier: process.env.GATSBY_VERIFIER_NAME,
+    typeOfLogin: 'google',
+    clientId: process.env.GATSBY_GOOGLE_CLIENT_ID
+  })
+
+  console.log(`userInfo : ${JSON.stringify(userInfo, null, 2)}`)
+}
+
+const Login = () => (
+  <button style={{ right: 0, position: 'absolute' }} onClick={login}>
+    Log in with tor.us
+  </button>
+)
+
+export default Login
