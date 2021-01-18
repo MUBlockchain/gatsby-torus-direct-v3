ece5a3ca90839bdde754b640850ece4da361a7ea
Jack Gilcrest <gilcest.jack@gmail.com>
2021-01-17 21:24:54 -0500

    env log 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,15 +6,16 @@ import UserProvider from '../components/auth'
 import AffiliateProvider from '../components/affiliate'
 
 const IndexPage = () => {
-  console.log("process.env", process.env)
+  console.log("BASE URL:", process.env.GATSBY_BASE_URL)
+  console.log("INFURA: ", process.env.GATSBY_INFURA)
   return (
     <UserProvider>
       <AffiliateProvider>
-    <Layout>
-      <SEO title='Home' />
-      <IndexBody />
-    </Layout>
-    </AffiliateProvider>
+        <Layout>
+          <SEO title='Home' />
+          <IndexBody />
+        </Layout>
+      </AffiliateProvider>
     </UserProvider>
   )
 }
